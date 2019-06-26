38ac6b2a105e16905fc4368e9e18ccd6fd7b37c3
Ankit Satyawali <ankitsatywali@gmail.com>
2019-06-26 22:23:51 +0530

    My backend

---
+++ app.js
@@ -0,0 +1,53 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const eventsRouter = require('./routes/events');
+const app = express();
+// database setup
+mongoose.set('useCreateIndex', true);
+mongoose.connect('mongodb+srv://sharejoy:fvPRubLxvULSryUd@cluster0-t9kzf.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+}).then(() => {
+	console.log('Connected to database');
+},
+err => {
+	console.log(err);
+    console.log('Connection Failed');
+});
+
+// view engine setup
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/uploads',express.static('uploads'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/events', eventsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({
+    message:err.message,
+    error:err
+  });
+});
+
+module.exports = app;
